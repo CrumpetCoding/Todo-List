84a7bab6e723490db1ce458d942a7797c512dfcb
CrumpetCoding <seannrowe@gmail.com>
2025-11-12 21:24:19 +0000

    Added local save function

--- index.js
+++ index.js
@@ -7,18 +7,30 @@ function addItem(){
         alert("Please write something.")
     }
     else{
-            let li = document.createElement("li");
-            li.innerHTML = text.value
-            list.appendChild(li);
-            let span = document.createElement("span");
-            span.innerHTML = "x";
-            li.appendChild(span);
+        let li = document.createElement("LI");
+        li.innerHTML = text.value
+        list.appendChild(li);
+        let span = document.createElement("SPAN");
+        span.innerHTML = "x";
+        li.appendChild(span);
     }
     text.value = "";
+    saveItems();
 }
 
 list.addEventListener("click", function(e){
     if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveItems();
     }
 }, false);
+
+function saveItems(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showList(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showList();
